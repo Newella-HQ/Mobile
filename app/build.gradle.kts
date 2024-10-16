9794b3d28e22e2c0fe980999eedbdef456185132
ploddasha <ploddasha@yandex.ru>
2024-10-16 13:59:21 +0700

    added some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,17 +53,39 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Compose
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // Навигация с Compose
+    implementation(libs.androidx.navigation.compose)
+
+    // Работа с сетью
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.kotlinxserialization)
+    implementation(libs.okhttp.logginginterceptor)
+
+    // Асинхронное программирование
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Тестирование
     testImplementation(libs.junit)
+    implementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    // Превью для Compose
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Сериализация JSON
+    implementation(libs.kotlinx.serialization.json)
+
 }
